69ccd0ff52be0bbf42a3e53cb358be178b890f22
Alice Frosi <afrosi@redhat.com>
2021-09-08 09:45:47 +0200

    Inital commit

---
+++ vendor/kubevirt.io/kubevirt/pkg/util/util.go
@@ -0,0 +1,125 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	v1 "kubevirt.io/client-go/api/v1"
+)
+
+const ExtensionAPIServerAuthenticationConfigMap = "extension-apiserver-authentication"
+const RequestHeaderClientCAFileKey = "requestheader-client-ca-file"
+const VirtShareDir = "/var/run/kubevirt"
+const VirtPrivateDir = "/var/run/kubevirt-private"
+const VirtLibDir = "/var/lib/kubevirt"
+const KubeletPodsDir = "/var/lib/kubelet/pods"
+const HostRootMount = "/proc/1/root/"
+const CPUManagerOS3Path = HostRootMount + "var/lib/origin/openshift.local.volumes/cpu_manager_state"
+const CPUManagerPath = HostRootMount + "var/lib/kubelet/cpu_manager_state"
+
+const NonRootUID = 107
+const NonRootUserString = "qemu"
+const RootUser = 0
+
+func IsNonRootVMI(vmi *v1.VirtualMachineInstance) bool {
+	_, ok := vmi.Annotations[v1.NonRootVMIAnnotation]
+	return ok
+}
+
+func IsSRIOVVmi(vmi *v1.VirtualMachineInstance) bool {
+	for _, iface := range vmi.Spec.Domain.Devices.Interfaces {
+		if iface.SRIOV != nil {
+			return true
+		}
+	}
+	return false
+}
+
+// Check if a VMI spec requests GPU
+func IsGPUVMI(vmi *v1.VirtualMachineInstance) bool {
+	if vmi.Spec.Domain.Devices.GPUs != nil && len(vmi.Spec.Domain.Devices.GPUs) != 0 {
+		return true
+	}
+	return false
+}
+
+// Check if a VMI spec requests VirtIO-FS
+func IsVMIVirtiofsEnabled(vmi *v1.VirtualMachineInstance) bool {
+	if vmi.Spec.Domain.Devices.Filesystems != nil {
+		for _, fs := range vmi.Spec.Domain.Devices.Filesystems {
+			if fs.Virtiofs != nil {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Check if a VMI spec requests a HostDevice
+func IsHostDevVMI(vmi *v1.VirtualMachineInstance) bool {
+	if vmi.Spec.Domain.Devices.HostDevices != nil && len(vmi.Spec.Domain.Devices.HostDevices) != 0 {
+		return true
+	}
+	return false
+}
+
+// Check if a VMI spec requests a VFIO device
+func IsVFIOVMI(vmi *v1.VirtualMachineInstance) bool {
+
+	if IsHostDevVMI(vmi) || IsGPUVMI(vmi) || IsSRIOVVmi(vmi) {
+		return true
+	}
+	return false
+}
+
+func NeedVirtioNetDevice(vmi *v1.VirtualMachineInstance, useEmulation bool) bool {
+	for _, iface := range vmi.Spec.Domain.Devices.Interfaces {
+		if !useEmulation && (iface.Model == "" || iface.Model == "virtio") {
+			return true
+		}
+	}
+	return false
+}
+
+// UseSoftwareEmulationForDevice determines whether to fallback to software emulation for the given device.
+// This happens when the given device doesn't exist, and software emulation is enabled.
+func UseSoftwareEmulationForDevice(devicePath string, useEmulation bool) (bool, error) {
+	if !useEmulation {
+		return false, nil
+	}
+
+	_, err := os.Stat(devicePath)
+	if err == nil {
+		return false, nil
+	}
+	if os.IsNotExist(err) {
+		return true, nil
+	}
+	return false, err
+}
+
+func ResourceNameToEnvVar(prefix string, resourceName string) string {
+	varName := strings.ToUpper(resourceName)
+	varName = strings.Replace(varName, "/", "_", -1)
+	varName = strings.Replace(varName, ".", "_", -1)
+	return fmt.Sprintf("%s_%s", prefix, varName)
+}
+
+// Checks if kernel boot is defined in a valid way
+func HasKernelBootContainerImage(vmi *v1.VirtualMachineInstance) bool {
+	if vmi == nil {
+		return false
+	}
+
+	vmiFirmware := vmi.Spec.Domain.Firmware
+	if (vmiFirmware == nil) || (vmiFirmware.KernelBoot == nil) || (vmiFirmware.KernelBoot.Container == nil) {
+		return false
+	}
+
+	return true
+}
+
+func HasHugePages(vmi *v1.VirtualMachineInstance) bool {
+	return vmi.Spec.Domain.Memory != nil && vmi.Spec.Domain.Memory.Hugepages != nil
+}
