560cc7573e83a07fabeb1ebbfa3661c6a730a5d7
Alice Frosi <afrosi@redhat.com>
2021-09-08 13:25:36 +0200

    Add utils

---
+++ pkg/utils/utils_test.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+
+	types "github.com/alicefr/kubevirt-hook-update-xml/pkg/types"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	vmSchema "kubevirt.io/client-go/api/v1"
+)
+
+var _ = Describe("KubevirtHook", func() {
+	BeforeEach(func() {
+		path, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		types.Dir = path
+	})
+
+	It("can be loaded from JSON", func() {
+		vmi := vmSchema.VirtualMachineInstance{
+			ObjectMeta: metav1.ObjectMeta{
+				UID: "5555938c-bf2d-48ef-a79c-7f52bba79f40",
+			},
+		}
+		bytes, err := json.Marshal(vmi)
+		Expect(err).NotTo(HaveOccurred())
+		MergeKubeVirtXMLWithProvidedXML("vmi-fedora.xml", bytes)
+	})
+})
