47c57590ad0a7ea5482b52f02b92c0b8e211b9a9
Alice Frosi <afrosi@redhat.com>
2021-09-14 11:14:16 +0200

    Update vendor

--- vendor/github.com/alicefr/kubevirt-hook-update-xml/pkg/utils/utils.go
+++ vendor/github.com/alicefr/kubevirt-hook-update-xml/pkg/utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	types "github.com/alicefr/kubevirt-hook-update-xml/pkg/types"
 	mxj "github.com/clbanning/mxj/v2"
@@ -32,19 +33,20 @@ func MergeKubeVirtXMLWithProvidedXML(file string, vmiJSON []byte) ([]byte, error
 		return []byte{}, err
 	}
 
-	var mv Map
-	mv, err = mxj.NewMapXml(rawXML)
+	mv, merr := mxj.NewMapXml(rawXML)
+	if merr != nil {
+		log.Log.Reason(merr).Errorf("Failed to unmarshal xml: %v", merr)
+		return []byte{}, merr
+
+	}
+	var v interface{}
+	v, err = mv.ValuesForPath("domain.metadata.kubevirt.uid")
 	if err != nil {
-		log.Log.Reason(err).Errorf("Failed to unmarshal xml: %v", err)
+		log.Log.Reason(err).Errorf("Failed parsing old uid in the xml value:%v : %v", v, err)
 		return []byte{}, err
-
 	}
-
-	if _, ok := objectMap["metadata"]; !ok {
-		return []byte{}, fmt.Errorf("Failed metedata not found")
-
-	}
-	fmt.Printf("XXX metadata %s \n", string(objectMap["metadata"]))
-	log.Log.Infof("New UID: %s", newUID)
-	return rawXML, nil
+	oldUID := v.([]interface{})[0].(string)
+	log.Log.Infof("Replace old UID:%s with new UID:%s", oldUID, newUID)
+	newXML := strings.ReplaceAll(string(rawXML), oldUID, newUID)
+	return []byte(newXML), nil
 }
